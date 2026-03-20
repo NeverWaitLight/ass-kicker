dbc9e48ea2f33bc5236ca5145e483d2ce63c647b
waitlight <waitlight@yeah.net>
2026-03-20 15:28:59 +0800

    refactor(template): change attributes field implementation in TemplateEntity
    - Updated the attributes field in TemplateEntity to use HashMap instead of LinkedHashMap for improved performance.
    - Ensured that the overall structure and functionality of the TemplateEntity remain intact while enhancing code efficiency.

--- backend/src/main/java/com/github/waitlight/asskicker/model/TemplateEntity.java
+++ backend/src/main/java/com/github/waitlight/asskicker/model/TemplateEntity.java
@@ -1,19 +1,20 @@
 package com.github.waitlight.asskicker.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -42,7 +43,7 @@ public class TemplateEntity {
     private ChannelType channelType;
 
     @Field("attributes")
-    private Map<String, String> attributes = new LinkedHashMap<>();
+    private Map<String, String> attributes = new HashMap<>();
 
     @Field("created_at")
     private Long createdAt;
