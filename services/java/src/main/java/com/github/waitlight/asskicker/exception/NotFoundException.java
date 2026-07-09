6c675aacedef2693118ee8d50470d7a9f4fdf710
waitlight <waitlight@yeah.net>
2026-07-09 14:35:53 +0800

    fix: resolve NotFoundException i18n message parsing issue
    - Remove conflicting constructor in NotFoundException that caused method overload ambiguity
    - Add template-related i18n message keys (English and Chinese)
    - Error messages now properly resolve through i18n after the fix

--- services/java/src/main/java/com/github/waitlight/asskicker/exception/NotFoundException.java
+++ services/java/src/main/java/com/github/waitlight/asskicker/exception/NotFoundException.java
@@ -4,10 +4,6 @@ package com.github.waitlight.asskicker.exception;
  * 资源不存在异常
  */
 public class NotFoundException extends BusinessException {
-    public NotFoundException(String resource, Object id) {
-        super("RESOURCE_NOT_FOUND", String.format("%s not found: %s", resource, id));
-    }
-
     public NotFoundException(String message) {
         super("RESOURCE_NOT_FOUND", message);
     }
